7354bbc4e9209d8954b4ea0524f4733685a3f27c
Omkar <saseomkar214@gmail.com>
2022-08-17 02:10:35 +0530

    17-8-2022

--- Codeforces/cp.cpp
+++ Codeforces/cp.cpp
@@ -2,33 +2,25 @@
 using namespace std;
 
 void solve(){
-   int n;cin>>n;
-   
-   vector<int>a(n);
-   unordered_map<int,int>m;
-
-   for(int i=0;i<n;i++){
-    cin>>a[i];
-    m[a[i]]++;
-   }
-   int cnt=0,pt=0;
-   for(int i=1;i<n;i++){
-     if(a[i]<a[i-1] or a[i]==0 or m[a[i]]==0){
-        int flag=true;
-        for(int j=pt;j<i;j++){
-            if(m[a[j]]==0)flag=false;
-            a[j]=0;
-            if(flag)cnt++;
-            m[a[j]]=0;
+    int n,k;cin>>n>>k;
+    
+    if((k%4)==0 and !(n&1)){
+        cout<<"NO"<<endl;
+        return;
+    }
+    
+    if(k&1){
+        for(int i=1;i<n;i+=2){
+            cout<<i<<" "<<i+1<<endl;
         }
-     }
-   }
-   for(auto i:a){
-    cout<<i<<" ";
-   }cout<<endl;
-   
-   cout<<cnt<<endl;
-   
+    }else{
+        for(int i=3;i<n;i+=4){
+            cout<<i<<" "<<i+1<<endl;
+        }
+        for(int i=1;i<n;i+=4){
+            cout<<i+1<<" "<<i<<endl;
+        }
+    }
 }
 
 int main()
